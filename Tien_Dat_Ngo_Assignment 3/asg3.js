f681088d3adcbaa5daf01d2191c66019626318ee
Tien Dat Ngo <phatdzhp@gmail.com>
2023-12-24 18:17:37 +1030

    asg3

--- Tien_Dat_Ngo_Assignment 3/asg3.js
+++ Tien_Dat_Ngo_Assignment 3/asg3.js
@@ -7,3 +7,41 @@ function toggleTheme() {
     root.setAttribute('data-theme', newTheme);
 }
 
+function updateHeading() {
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const userNameFromQuery = urlParams.get('user_name');
+
+    
+    const storedTheme = localStorage.getItem('theme');
+    const currentTheme = storedTheme || 'light';
+
+    
+    document.body.setAttribute('data-theme', currentTheme);
+
+    
+    const userName = userNameFromQuery || localStorage.getItem('user_name') || 'Tien Dat Ngo';
+
+    
+    const greetingElement = document.getElementById('greeting');
+    greetingElement.textContent = `Hello, ${userName} - Software Engineer`;
+}
+
+
+function toggleTheme() {
+    const root = document.documentElement;
+    const currentTheme = root.getAttribute('data-theme') || 'light';
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    
+    localStorage.setItem('theme', newTheme);
+
+    
+    root.setAttribute('data-theme', newTheme);
+}
+
+
+document.getElementById('nameForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    updateHeading();
+});
